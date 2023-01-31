d279a077a68c4a54e811053e468446870976efb4
Carson Long <lcarson@vmware.com>
2023-01-31 15:48:13 -0800

    Omit comparison to bool field

--- cfCmdGenerator/cfCmdGenerator.go
+++ cfCmdGenerator/cfCmdGenerator.go
@@ -124,7 +124,7 @@ func (c *cfCmdGenerator) Push(name, path string, instances int) cmdStartWaiter.C
 		"-i", strconv.Itoa(instances),
 	}
 
-	if c.useBuildpackDetection == false {
+	if !c.useBuildpackDetection {
 		args = append(args, "-b", "go_buildpack")
 	}
 
